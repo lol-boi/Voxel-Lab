9d03f3d08c8a1d126c9d08f90d347f7a07911029
lol-boi <amanwastakenwastaken1@gamil.com>
2024-07-24 18:57:45 +0530

    now the win .exe should work

---
+++ build/build_win/CMakeFiles/image.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image.dir/libs/stb_image.c.obj"
+  "CMakeFiles/image.dir/libs/stb_image.c.obj.d"
+  "libimage.dll"
+  "libimage.dll.a"
+  "libimage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
