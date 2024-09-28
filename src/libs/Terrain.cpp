7dafcc7f1346e1b52e36fdba8ce9060a975b82bb
lol-boi <amanwastakenwastaken1@gmail.com>
2024-09-29 02:04:01 +0530

    Worked on the infinite terrain generator Used vector and updated the terrain everytime the player moves to next chunk currenty renders the whole terrain completly.

--- src/libs/Terrain.cpp
+++ src/libs/Terrain.cpp
@@ -5,20 +5,20 @@
 #include <glm/ext/vector_float2.hpp>
 #include <glm/ext/vector_float4.hpp>
 #include <glm/ext/vector_int2.hpp>
-#include <unordered_map>
+#include <mutex>
 #include <vector>
 #include "Chunk.hpp"
 #include <cmath>
+#include <iostream>
 
 Terrain::Terrain(int no_of_chunks,int seed,  int c_size = 32){
-    if(no_of_chunks % 2 != 0){
-        no_of_chunks += 1;
-    }
-    world_size_in_chunks  = no_of_chunks;
+    no_of_chunks += 1;
     chunk_size = c_size;
     world_size = chunk_size * no_of_chunks;
     world_seed = seed;
-    prev_pos = glm::vec2(0,0);
+    render_distance = 3;
+    prev_chunk_pos = glm::ivec2(0,0);
+    is_buffer_updated = false;
     vertices = {
         -0.5f, -0.5f, 0.5f,
          0.5f, -0.5f, 0.5f,
@@ -40,7 +40,6 @@ Terrain::Terrain(int no_of_chunks,int seed,  int c_size = 32){
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE,sizeof(float)*3, (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -50,48 +49,46 @@ Terrain::Terrain(int no_of_chunks,int seed,  int c_size = 32){
     glVertexAttribDivisor(1,1);
 
     glBindVertexArray(0);
+
+    glBindBuffer(GL_DRAW_INDIRECT_BUFFER, indirect_buffer);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+
 }
 
 
-void Terrain::init_world_chunks(glm::vec3 cam_pos){
-    chunk_positions.clear();
-    int render_distance = 4;
-    int x_chunk, z_chunk;
-    x_chunk = (int)cam_pos.x / 32;
-    z_chunk = (int) cam_pos.y / 32;
-    if(prev_pos == glm::vec2(x_chunk,z_chunk)){
-       return;
-    }
-    for(int x = x_chunk - render_distance; x <= x_chunk + render_distance; x++ ){
-        for(int z = z_chunk - render_distance; z <= z_chunk + render_distance; z++){
-            for(int y = 0; y<8; y++){
-                Chunk chunk = Chunk(glm::vec3(x,y,z));
-                chunk.gen_chunk_data(chunk_size, world_seed);
-                chunkmap.emplace(glm::vec3(x,y,z), chunk);
-                chunk_positions.push_back(glm::vec4(x,y,z,0));
+bool Terrain::init_world_chunks(glm::vec3 cam_pos){
+    int x_chunk = (int)cam_pos.x / 32;
+    int z_chunk = (int) cam_pos.z / 32;
+
+    if(prev_chunk_pos != glm::ivec2(x_chunk,z_chunk)){
+        std::cout << "Player now at [" << x_chunk << "," << z_chunk<< "]" <<std::endl;
+        std::lock_guard<std::mutex> lock(buffer_mutex);
+        chunk_positions.clear();
+        chunks_data.clear();
+        for(int x = x_chunk - render_distance; x <= x_chunk + render_distance; x++ ){
+            for(int z = z_chunk - render_distance; z <= z_chunk + render_distance; z++){
+                for(int y = 0; y<8; y++){
+                    Chunk chunk = Chunk(glm::vec3(x,y,z));
+                    chunk.gen_chunk_data(chunk_size, world_seed);
+                    chunks_data.push_back(chunk);
+                    chunk_positions.push_back(glm::vec4(x,y,z,0));
+                }
             }
         }
+        prev_chunk_pos = glm::vec2(x_chunk,z_chunk);
+        update_buffer_data();
+        return true;
     }
-    //for(int x = 0; x<world_size_in_chunks; x++){
-    //    for(int y = 0; y<8; y++){
-    //        for(int z = 0; z<world_size_in_chunks; z++){
-    //           Chunk chunk = Chunk(glm::vec3(x,y,z));
-    //           chunk.gen_chunk_data(chunk_size,world_seed);
-    //           chunkmap.emplace(glm::vec3(x,y,z), chunk);
-    //        }
-    //    }
-    //}
+    return false;
 }
 
-void Terrain::update_terrain(){
+void Terrain::update_buffer_data(){
     draw_commands.clear();
     instance_data.clear();
 
-    int offset_size = 0;
+    size_t offset_size = 0;
 
-    for(auto &entry: chunk_positions){
-
-        Chunk &chunk = chunkmap.at(glm::vec3(entry.x,entry.y,entry.z));
+    for(auto &chunk : chunks_data){
         chunk.gen_mesh(chunk_size, &instance_data);
 
         DrawArraysIndirectCommand cmd;
@@ -102,13 +99,21 @@ void Terrain::update_terrain(){
         draw_commands.push_back(cmd);
         offset_size = instance_data.size();
     }
+
+    std::cout << "Updated the buffers" << std::endl;
+    is_buffer_updated = true;
+}
+
+void Terrain:: upload_buffers(){
+    std::lock_guard<std::mutex> lock(buffer_mutex);
     glBufferData(GL_SHADER_STORAGE_BUFFER, chunk_positions.size() * sizeof(glm::vec4), chunk_positions.data(), GL_DYNAMIC_DRAW);
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
 
     glBufferData(GL_DRAW_INDIRECT_BUFFER, draw_commands.size() * sizeof(DrawArraysIndirectCommand),draw_commands.data(), GL_DYNAMIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER,ibo);
-    glBufferData(GL_ARRAY_BUFFER,instance_data.size() * sizeof(int), instance_data.data(),GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER,instance_data.size() * sizeof(int), instance_data.data(),GL_DYNAMIC_DRAW);
+    std::cout << "Uploaded the buffers" << std::endl;
+    is_buffer_updated = false;
 }
 
 void Terrain::draw_terrain(){
